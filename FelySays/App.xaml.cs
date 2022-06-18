476943b13086cb9e5f4e67ed5434eca8c187bb82
chieper <chieper@gmail.com>
2022-06-18 19:46:55 +1000

    Add project files.

---
+++ FelySays/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FelySays;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ FelySays/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FelySays;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
